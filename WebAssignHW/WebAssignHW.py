fe160d6b68a0e67df47ea218780febaa33bbf185
Camilo Lozano <camilozano98@gmail.com>
2017-10-03 18:19:38 -0400

    Writes to an ICS file. Issue with parsing dates in local time not UTIC

--- WebAssignHW/WebAssignHW.py
+++ WebAssignHW/WebAssignHW.py
@@ -2,15 +2,9 @@ from selenium						import webdriver
 from selenium.webdriver.common.keys	import Keys
 from selenium.webdriver.support.ui	import Select
 from dateutil.parser				import parse
-import time
-
-#Read user and pass from file
-file = open('account.key','r')
-key = file.readlines()
-url		= key[0].replace('url=','')
-user	= key[1].replace('user=','')
-pwd		= key[2].replace('pass=','')
-file.close()
+from datetime						import timedelta
+from ics							import Calendar, Event
+import pytz
 
 #Given a Webassign course page return homework 
 #Return list in format {'assignment':'datetime '}
@@ -21,39 +15,57 @@ def get_hw(selenium, course):
 	hw.pop('Name')
 	return hw
 
-#Sets up browser
-selenium = webdriver.Chrome()
-selenium.get(url)
-selenium.implicitly_wait(5)
+#Returns compiled list of homework of all classes
+def all_homework(url, user, pwd):
+	#Sets up browser
+	selenium = webdriver.Chrome()
+	selenium.get(url)
+	selenium.implicitly_wait(5)
 
-#Sends username and pass
-userfield=selenium.find_element_by_id("email").send_keys(user)
-pwdfield= selenium.find_element_by_id("cengagePassword").send_keys(pwd)
+	#Sends username and pass
+	userfield=selenium.find_element_by_id("email").send_keys(user)
+	pwdfield= selenium.find_element_by_id("cengagePassword").send_keys(pwd)
 
-#Counter index for list of classes as first two options aren't courses
-class_num = 2
+	#Counter index for list of classes as first two options aren't courses
+	class_num = 2
 
-homework={}
+	homework={}
 
-#Loops thru dropdown menu 
-while(True):
-		select = selenium.find_element_by_id('course') #Gathers array of courses
-		options = select.find_elements_by_tag_name("option") #Gets lenght for break 
-		course=options[class_num].text
-		options[class_num].click()
-		homework[course]=get_hw(selenium,course) #stores assingments in form of {'class':{'hw','due'}}
-		class_num += 1
-		selenium.back()
-		if(class_num > len(options)-1): break
+	#Loops thru dropdown menu 
+	while(True):
+			select = selenium.find_element_by_id('course') #Gathers array of courses
+			options = select.find_elements_by_tag_name("option") #Gets lenght for break 
+			course=options[class_num].text
+			options[class_num].click()
+			homework[course]=get_hw(selenium,course) #stores assingments in form of {'class':{'hw','due'}}
+			class_num += 1
+			selenium.back()
+			if(class_num > len(options)-1): break
+	selenium.close()
+	return homework
 
-selenium.close()
+
+
+#Read user and pass from file
+file = open('account.key','r')
+key = file.readlines()
+url		= key[0].replace('url=','')
+user	= key[1].replace('user=','')
+pwd		= key[2].replace('pass=','')
+file.close()
+
+homework = all_homework(url,user,pwd) #Grabs all homework onto list
+
+cal = Calendar()
 
 for course, assignment in homework.items():
-    for homework, due in assignment.items():
-            print('{}: ({},{})'.format(course, homework, due))
+	for homework, due in assignment.items():
+		time = parse(due).astimezone(pytz.es)
+		print('{} {} {}'.format(homework, due, time))
+		cal.events.append(Event(name=homework,begin=time-timedelta(hours=1),end=time,description=course))
 
+with open('WebAssign.ics', 'w') as f:
+    f.writelines(cal)
 
-
-
-
-
+import time
+time.
